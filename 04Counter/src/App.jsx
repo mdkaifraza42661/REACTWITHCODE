035ccea709508ef7f41c23e57e49c79d4aaa66a0
mdkaifraza42661 <kaifraza42661@gmail>
2024-01-02 11:50:55 +0530

    CounterApp basic react first projectcounter using useState and donot goto lessthan 0 and more than twenty in counter

---
+++ 04Counter/src/App.jsx
@@ -0,0 +1,41 @@
+import {useState} from 'react'
+
+import './App.css'
+
+function App() {
+  let [Counter, setCounter] = useState(15)
+  // let [Countersub, setCountersub] = useState({Counter})
+  // console.log(Countersub);
+
+  // let Counter = 5
+  const updatevalue = () => {
+    // Counter = Counter + 1
+    if(Counter < 20) {
+      // setCounter(0)
+      setCounter(Counter + 1)
+    }
+    // console.log(Counter = Counter + 1);
+    // console.log("upvalue", Math.random());
+  }
+  const decvalue = () => {
+    if(Counter > 0) {
+
+      setCounter(Counter - 1)
+    }
+    // console.log(Countersub - 1)
+    // setCountersub(Countersub - 1)
+    // console.log(Counter = Counter - 1);
+    // console.log("lowvalue", Math.random());
+  }
+
+  return (
+    <>
+      <h1>Chai or code</h1>
+      <h2>update value:{Counter}</h2>
+      <button onClick={updatevalue}>Add Value</button>
+      <button onClick={decvalue}>subs Value</button>
+    </>
+  )
+}
+
+export default App
